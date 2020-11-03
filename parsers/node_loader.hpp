3184f2eb68e3740dd90e81fe8b5e25ec04998dd0
Jackson Ming Hu <huming2207@gmail.com>
2020-11-03 21:14:39 +1100

    Implement basic loader stategy

---
+++ parsers/node_loader.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <cstddef>
+#include <esp_log.h>
+
+#include "pugixml.hpp"
+
+#include "defs.hpp"
+#include "node_vector.hpp"
+
+#define TAG "node_loader"
+
+namespace mb
+{
+    class node_loader
+    {
+    public:
+        def::state_t load_in_place(const char *xml_str, size_t len)
+        {
+            auto result = xml_doc.load_buffer_inplace((void *) xml_str, len);
+            if (result.status == pugi::status_out_of_memory || result.status == pugi::status_io_error) {
+                ESP_LOGE(TAG, "No memory or invalid I/O states for XML loading");
+                return def::STATE_NO_MEM;
+            } else if (result.status != pugi::status_ok) {
+                ESP_LOGE(TAG, "Got invalid states from PugiXML: 0x%x", result.status);
+                return def::STATE_INVALID_XML;
+            }
+
+            ESP_LOGI(TAG, "XML document loaded");
+
+            // Find the main node
+            auto mb_node = xml_doc.child("magic_brush");
+            if (mb_node.empty()) {
+                ESP_LOGE(TAG, "No child found for the main node");
+                return def::STATE_INVALID_XML;
+            }
+
+            // Probe length and reserve memory in advance - to avoid heap fragmentation
+            size_t xml_len = std::distance(mb_node.begin(), mb_node.end());
+            if (xml_len < 1) {
+                ESP_LOGE(TAG, "No children node found");
+                return def::STATE_INVALID_XML;
+            }
+
+            ESP_LOGI(TAG, "XML loaded, node count: %u", xml_len);
+            nodes.reserve(xml_len);
+
+            // Iterate through XML doc
+            auto ret = def::STATE_OK;
+            for (auto& node : mb_node.children()) {
+                ret = nodes.parse_node(node);
+                if (ret != def::STATE_OK) return ret;
+            }
+
+            ESP_LOGI(TAG, "Done loading");
+            return ret;
+        }
+
+    private:
+        pugi::xml_document xml_doc = {};
+        node_vector nodes = {};
+    };
+}
