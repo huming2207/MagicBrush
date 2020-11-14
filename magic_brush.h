bf871e7bb148fdb17a02dcc99e55054c71524025
Jackson Ming Hu <huming2207@gmail.com>
2020-11-14 22:01:45 +1100

    Rename base style

---
+++ magic_brush.h
@@ -0,0 +1,9 @@
+#pragma once
+
+extern "C"
+{
+    void mb_init()
+    {
+
+    }
+}
---
+++ parsers/styles/base_style.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "external/pugixml/pugixml.hpp"
+#include "helper/defs.hpp"
+#include "lvgl.h"
+
+namespace mb::style
+{
+    class base_style
+    {
+    public:
+        virtual const char *name() = 0;
+        virtual def::state_t parse(const pugi::xml_node &node, lv_style_t *style) = 0;
+    };
+}
